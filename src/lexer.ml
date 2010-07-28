b1136fc46933433e2a4eba8c5c6e85d8a3eaec44
a_m0d <damien.schoof@gmail.com>
2010-07-28 12:38:32 +0800

    Fleshed out the lexer a bit more - it now recognises comments and identifiers

--- src/lexer.ml
+++ src/lexer.ml
@@ -1,13 +1,39 @@
-open Token
 
 let rec lex ?(block = false) = parser
-    | [< ' (' '); stream >] -> lex stream
-    | [< ' ('\n') >] -> [< >]
-    | [< ' ('%'); stream >] -> lex_comment stream
-    | [< 'c; stream >] -> [< 'Token.Op c; lex stream >]
-    | [< >] -> raise End_of_file
+    (* Whitespace *)
+    | [< ' (' '); stream >] -> 
+            lex stream
+    (* Newlines *)
+    | [< ' ('\n') >] -> 
+            [< >]
+    (* Comments *)
+    | [< ' ('%'); stream >] -> 
+            lex_comment stream
+    (* Identifiers *)
+    | [< ' ('a'..'z'|'A'..'Z'|'_') as c; stream >] ->
+            let buffer = Buffer.create 1 in
+            Buffer.add_char buffer c;
+            lex_ident buffer stream
+    (* Anything else *)
+    | [< 'c; stream >] -> 
+            [< 'Token.Op c; lex stream >]
+    (* End of the stream *)
+    | [< >] -> 
+            raise End_of_file
+
+and lex_ident buffer = parser
+    | [< ' ('a'..'z'|'A'..'Z'|'0'..'9'|'-'|'_') as c; stream >] ->
+            Buffer.add_char buffer c;
+            lex_ident buffer stream
+    | [< stream = lex >] -> 
+            match Buffer.contents buffer with
+            | "def" -> [< 'Token.Def; stream >]
+            | id -> [< 'Token.Ident id; stream >]
 
 and lex_comment = parser
-    | [< ' ('\n'); stream = lex >] -> stream
-    | [< 'c; stream = lex_comment >] -> stream
-    | [< >] -> [< >]
+    | [< ' ('\n'); stream = lex >] -> 
+            stream
+    | [< 'c; stream = lex_comment >] -> 
+            stream
+    | [< >] -> 
+            [< >]
