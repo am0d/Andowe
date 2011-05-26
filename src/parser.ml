4d7bc4694760df46cf04d3c900a55617b9bc5962
a_m0d <damien.schoof@gmail.com>
2011-05-25 20:27:02 -0400

    Not quite following the kaleidoscope example here, but added float support as well

--- src/parser.ml
+++ src/parser.ml
@@ -3,8 +3,16 @@ open Syntax
 let rec toplevel = parser
     | [< >] -> []
 
+(* primary
+ *      ::= numberexpr
+ *)
 and parse_primary = parser
+    (* numberexpr
+     *      ::= intexpr
+     *      ::= floatexpr
+     *)
     | [< 'Token.Int i >] -> Number (float_of_int i)
+    | [< 'Token.Float f>] -> Number f
     | [< >] -> raise (Message.Error "Number expected")
 
 and parse_expr = parser
